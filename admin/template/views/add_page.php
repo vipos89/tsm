2c7b85d3261b9555b7f92dcaeb37a60ba4cdf5cf
vipos89 <webmaster.kobrin@gmail.com>
2020-11-25 22:12:16 +0300

    init

--- admin/template/views/add_page.php
+++ admin/template/views/add_page.php
@@ -1,10 +1,21 @@
+<?php
+if (isset($_GET['id']) && !empty($_GET['id']) && $_GET['action'] == 'edit_page') {
+    $sql = "SELECT * from pages where id = " . $_GET['id'];
+    $res = mysqli_query($connection, $sql);
+    $page = mysqli_fetch_assoc($res);
+}
+
+$url = isset($page['id']) ? '/admin/?action=update_page&id=' . $page['id'] : '/admin/?action=save_page';
+
+
+?>
 <div id="page-wrapper">
     <div class="row">
-        <form role="form" method="post" action="/admin/?action=save_page" enctype="multipart/form-data">
+        <form role="form" method="post" action="<?= $url ?>" enctype="multipart/form-data">
 
             <div class="form-group">
                 <label>Text Input</label>
-                <input class="form-control" name="title">
+                <input class="form-control" name="title" value="<?= $page['title'] ?? '' ?>">
                 <p class="help-block">Example block-level help text here.</p>
             </div>
 
@@ -16,21 +27,22 @@
 
             <div class="form-group">
                 <label>Text Input</label>
-                <input class="form-control" name="author">
+                <input class="form-control" name="author" value="<?= $page['author'] ?? '' ?>">
                 <p class="help-block">Example block-level help text here.</p>
             </div>
 
             <div class="form-group">
                 <label>Text Input</label>
-                <input class="form-control" name="category">
+                <input class="form-control" name="category" value="<?= $page['category'] ?? '' ?>">
                 <p class="help-block">Example block-level help text here.</p>
             </div>
 
             <div class="form-group">
                 <label>Text area</label>
-                <textarea class="form-control" rows="3" name="content"></textarea>
+                <textarea class="form-control" rows="3" name="content">
+                    <?= $page['content'] ?? '' ?>
+                </textarea>
             </div>
-
 
 
             <button type="submit" class="btn btn-default">Submit Button</button>
